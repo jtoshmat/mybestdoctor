3892129b1afd32446709e8bac4b38b9cc20fa815
sher <friend802000@gmail.com>
2020-04-19 19:25:18 -0400

    saving DATABASE

---
+++ database/migrations/2020_04_19_024152_create_doctor_lists_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_lists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('doctors_name', 100);
+            $table->char('speciality_type', 100);
+            $table->char('speciality_title', 100);
+            $table->text('spec_desc1');
+            $table->text('spec_desc2');
+            $table->text('spec_desc3');
+            $table->text('spec_desc4');
+            $table->text('location');
+            $table->float('rate', 8, 2);
+            $table->char('price_range', 100);
+            $table->text('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_lists');
+    }
+}
