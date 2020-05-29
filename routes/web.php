a13ef1d1b23c21fe16e700af52298157eaa8134e
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-05-28 21:58:07 -0400

    updated the route.web

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| Updated by Jon Toshmatov on May 28, 2020 9:57 PM
 |
 */
 
